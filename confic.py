e5422758959bd65f21372276b8e1bafde54f6537
AntonKumets <kumetsanton@gmail.com>
2026-02-06 14:16:35 +0200

    Update confic.py

--- confic.py
+++ confic.py
@@ -1,4 +1,5 @@
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATABASE = os.path.join(BASE_DIR, "my_projects.db")
-token = "7795964246:AAG8d4ul5P6m6n76oH8SZgNeNqPCwnIweXM"
+
+token = ""
